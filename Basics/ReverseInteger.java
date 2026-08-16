c16e75d5eb1ed5d3eb5a0aea6556661203d909bf
Priyanshu <priyanshu924429@gmail.com>
2026-08-16 21:34:00 +0530

    Reverse Integer Leetcode Solution

--- Basics/ReverseInteger.java
+++ Basics/ReverseInteger.java
@@ -1,17 +1,33 @@
-import java.util.Scanner;
-public class ReverseInteger {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the integer to be reversed: ");
-        int num = sc.nextInt();
-        int digit;
-        int rev=0;
-        while(num>0){
-            digit = num%10;
-            rev= rev*10+digit;
-            num /=10;
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while (x != 0) {
+            int digit = x % 10; 
+            x /= 10;
+            rev = rev * 10 + digit;
+            if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
+                return 0;
+            }
         }
-        System.out.println("Here is reversed ingeger: "+rev);
-        sc.close();
-    }   
+        return (int) rev;
+    }
 }
+// Earlier VS Code Solution -
+
+// import java.util.Scanner;
+// public class ReverseInteger {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the integer to be reversed: ");
+//         int num = sc.nextInt();
+//         int digit;
+//         int rev=0;
+//         while(num>0){
+//             digit = num%10;
+//             rev= rev*10+digit;
+//             num /=10;
+//         }
+//         System.out.println("Here is reversed ingeger: "+rev);
+//         sc.close();
+//     }   
+// }
