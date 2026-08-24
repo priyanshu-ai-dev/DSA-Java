2e4b19bede02895e422ec0bc22b6c200d623d5d4
Priyanshu <priyanshu924429@gmail.com>
2026-08-24 21:59:14 +0530

    Excel Sheet Column Title Leetcode 168 Solution

---
+++ Math/ExcelColumnTitle.java
@@ -0,0 +1,14 @@
+public class ExcelColumnTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        
+        while (columnNumber > 0) {
+            columnNumber--;               
+            int remainder = columnNumber % 26;
+            sb.append((char) ('A' + remainder));
+            columnNumber /= 26;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
