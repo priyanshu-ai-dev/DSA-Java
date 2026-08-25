e0d8dd176ca20f5601bbf67e94b8cc6bee079788
Priyanshu <priyanshu924429@gmail.com>
2026-08-25 21:57:48 +0530

    Valid Perfect Square LeetCode 367 Solution

--- Math/ValidPerfectSquare.java
+++ Math/ValidPerfectSquare.java
@@ -7,38 +7,31 @@ class ValidPerfectSquare {
         //     }
         // }
         // return false;
-        // Got a pattern of perfect square
-        // Every perfect square end digit is 0,1,4,5,6,9 .
-        // double value = Math.sqrt(num);
-        // int value1 = (int)value;
-        // if(value>value1){
-        //     return false;
-        // }
-        // return true;
+        // Working Solution 1 Binary Search Method
         if (num < 1) return false;
-        long x = num;
-        while (x * x > num) {
-            x = (x + num / x) / 2;
+        
+        long lo = 1, hi = num;
+        while (lo <= hi) {
+            long mid = lo + (hi - lo) / 2;
+            long sq = mid * mid;
+            
+            if (sq == num) {
+                return true;
+            } else if (sq < num) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
-        return x * x == num;
+        return false;
+
+
+        // Working Solution 2
+        // if (num < 1) return false;
+        // long x = num;
+        // while (x * x > num) {
+        //     x = (x + num / x) / 2;
+        // }
+        // return x * x == num;
     }
 }
-// import java.util.Scanner;
-// public class Valid_Perfect_Square {
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Enter the number to check: ");
-//         int num = sc.nextInt();
-//         boolean flag = false;
-//         for(int i =0;i*i<=num;i++){
-//             if(i*i == num){
-//                 flag = true;
-//                 System.out.println(num+" is a valid perfect square of "+i);
-//             }
-//         }
-//         if(flag == false){
-//         System.out.println(num+" is not a valid perfect Square.");
-//         }
-//         sc.close();
-//     }
-// }
