789a21bdd200170e38516abc54e0dff867672820
Priyanshu <priyanshu924429@gmail.com>
2026-08-28 03:14:45 +0530

    Count the Number of Special Characters LeetCode 3120 Solution

---
+++ Strings/CountSpecialCharacters.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class CountSpecialCharacters {
+    public int numberOfSpecialChars(String word) {
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        for (char c : word.toCharArray()) {
+
+            if (c >= 'a' && c <= 'z') {
+                lower[c - 'a'] = true;
+            } else {
+                upper[c - 'A'] = true;
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
