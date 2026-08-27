71da1ceec15ab64eb22c1e64ca311b5b5a69cd12
Priyanshu <priyanshu924429@gmail.com>
2026-08-27 23:46:56 +0530

    Count Primes LeetCode 204 Solution

--- Basics/CountPrime.java
+++ Basics/CountPrime.java
@@ -1,30 +1,53 @@
-import java.util.Scanner;
-
-public class CountPrime {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number to check for prime:");
-        int num = sc.nextInt();
-        int countp = 0;
-        System.out.println("Prime Numbers in the range of 1 to "+num+" :");
-        for (int j = 2; j <= num; j++) {
-            int count = 0;
-            for (int i = 2; i <= Math.sqrt(j); i++) {
-                if (j % i == 0) {
-                    count++;
+// Optimized Approach for LeetCode 
+class CountPrime {
+    public int countPrimes(int n) {
+        if (n < 3) return 0;
+        
+        boolean[] isComposite = new boolean[n];  // false = "not yet known to be composite"
+        int count = 0;
+        
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                // mark all multiples of i as composite
+                for (long j = (long) i * i; j < n; j += i) {
+                    isComposite[(int) j] = true;
                 }
             }
-
-            if (j == 2 || count == 0) {
-                System.out.println(j);
-                countp++;
-
-            }
         }
-        System.out.print("Total Prime Numbers in the range of 1 to "+num+" is ");
-        System.out.print(countp);
-        System.out.println("");
-        sc.close();
-
+        
+        return count;
     }
 }
+
+
+// import java.util.Scanner;
+
+// public class CountPrime {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number to check for prime:");
+//         int num = sc.nextInt();
+//         int countp = 0;
+//         System.out.println("Prime Numbers in the range of 1 to "+num+" :");
+//         for (int j = 2; j <= num; j++) {
+//             int count = 0;
+//             for (int i = 2; i <= Math.sqrt(j); i++) {
+//                 if (j % i == 0) {
+//                     count++;
+//                 }
+//             }
+
+//             if (j == 2 || count == 0) {
+//                 System.out.println(j);
+//                 countp++;
+
+//             }
+//         }
+//         System.out.print("Total Prime Numbers in the range of 1 to "+num+" is ");
+//         System.out.print(countp);
+//         System.out.println("");
+//         sc.close();
+
+//     }
+// }
