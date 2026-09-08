ba18b6821e1b76e32acb02aec36cbd0a09b04e13
Priyanshu <priyanshu924429@gmail.com>
2026-09-08 23:58:48 +0530

    Reverse Words in a String LeetCode 557 Solution

---
+++ Strings/ReverseWordsString.java
@@ -0,0 +1,30 @@
+class ReverseWordsString {
+
+    public String reverseWords(String s) {
+
+        String result = "";
+        String intermediate = "";
+
+        for (char c : s.toCharArray()) {
+
+            if (c != ' ') {
+
+                intermediate += c;
+
+            } else {
+
+                String imrev = new StringBuilder(intermediate).reverse().toString();
+
+                result = result + imrev + " ";
+
+                intermediate = "";
+            }
+        }
+
+        // Process last word
+        String imrev = new StringBuilder(intermediate).reverse().toString();
+        result = result + imrev;
+
+        return result;
+    }
+}
